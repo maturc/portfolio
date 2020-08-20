935608ac447d33710227712e5badbad16c1febce
Matija Turčec <turcec.matija@gmail.com>
2020-08-20 23:14:16 +0200

    basic layout of the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CssBaseline, Container } from '@material-ui/core';
+import Intro from './components/Intro';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container>
+        <Intro />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        {/* footer?? - quick nav links? created by and stuff??
+        back to top button? */}
+      </Container>
+    </>
   );
 }
 
