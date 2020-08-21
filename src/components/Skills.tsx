512866f305f61eb6fbed8640b82e7f09c9cbf77e
Matija Turčec <turcec.matija@gmail.com>
2020-08-21 23:07:47 +0200

    updated skills componenet

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 function Skills() {
   return (
-    <>
-    Web development skills that I have experience with:
-    Technologies:
-      JavaScript, Typescript, Node, Git, SASS, CSS, HTML, jQuery
-    Frameworks:
-      React, Express, Material UI
-    Database:
-      MySQL, MongoDB
-    Bundlers:
-      Webpack
-    Tools:
-      Photoshop, Microsoft Office, Open Office
-    </>
+    <section className="section">
+      <Typography variant="h2">
+        Web development skills
+      </Typography>
+      <Typography variant="body1" component="ul">
+        <li>Technologies:</li>
+          <ul>
+            <li>JavaScript, Typescript, Node, Git, SASS, CSS, HTML, jQuery</li>
+          </ul>
+        <li>Frameworks:</li>
+          <ul>
+            <li>React, Express, Material UI</li>
+          </ul>
+        <li>Database:</li>
+          <ul>
+            <li>MySQL, MongoDB</li>
+          </ul>
+        <li>Bundlers:</li>
+          <ul>
+            <li>Webpack</li>
+          </ul>
+        <li>Tools:</li>
+          <ul>
+            <li>Photoshop, Microsoft Office, Open Office</li>
+          </ul>
+        <li>BEM methodology?</li>
+      </Typography>
+    </section>
   );
 }
 
