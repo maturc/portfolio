2c29d56c8c41ed83eefec64e825f5ea6f23d23ce
Matija Turčec <turcec.matija@gmail.com>
2020-08-22 23:52:33 +0200

    added a custom hook for navbar highlighting

---
+++ src/hooks/useSectionInView.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+
+
+function useSectionInView( sections: Array<HTMLElement>, navArray: Array<HTMLElement> ) {
+  const options = {
+    root: null,
+    teshold: 1,
+    rootMargin: "-10% 0px -80%"
+  }
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach( entry => {
+          if( entry.isIntersecting ) {
+            navArray.forEach( (element: HTMLElement) => {
+              element.classList.remove("test")
+            });
+            if( entry.target.id !== "intro" )
+              navArray[Number(entry.target.id)].classList.toggle("test");
+          }
+        });
+      }, options );
+      sections.forEach( (section: HTMLElement) => {
+        observer.observe(section);
+      });
+      return () => {
+        sections.forEach( (section: HTMLElement) => {
+          observer.unobserve(section);
+        });
+      };
+  }, [sections, navArray, options]);
+}
+
+export default useSectionInView;
