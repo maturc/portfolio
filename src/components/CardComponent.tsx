8e62106298c353c415cda8cd58a878b7fba21460
Matija Turčec <turcec.matija@gmail.com>
2020-08-25 23:59:09 +0200

    added buttons to cards

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import { Paper, Typography, Button } from '@material-ui/core';
 import { ICard } from '../interfaces';
 
-function CardComponent( {imgUrl, imgAlt, title, body}:ICard ) {
+function CardComponent( {imgUrl, imgAlt, title, body, linkToWebsite, linkToSource}:ICard ) {
   return (
     <Paper elevation={1} className="card">
       <img src={imgUrl} alt={imgAlt} className="card__img" />
@@ -13,6 +13,24 @@ function CardComponent( {imgUrl, imgAlt, title, body}:ICard ) {
         <Typography variant="body1">
           {body}
         </Typography>
+        <div className="card__buttons">
+          <Button
+            onClick={() => window.open(linkToWebsite, "_blank")}
+            className="button"
+            color="primary" variant="contained"
+            disableRipple={true}
+          >
+            Open Website
+          </Button>
+          <Button
+            onClick={() => window.open(linkToSource, "_blank")}
+            className="button"
+            color="primary" variant="contained"
+            disableRipple={true}
+          >
+            Source Code
+          </Button>
+        </div>
       </div>
     </Paper>
   );
