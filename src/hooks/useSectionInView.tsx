ea3cef14aa521097e47a79ffdd60cb678faccd59
Matija Turčec <turcec.matija@gmail.com>
2020-08-26 00:01:25 +0200

    changed intersection shape, added animation class

--- src/hooks/useSectionInView.tsx
+++ src/hooks/useSectionInView.tsx
@@ -5,19 +5,22 @@ import { ISectionInView } from "../interfaces";
 const useSectionInView: ISectionInView = (sections, navArray) => {
   const options = {
     root: null,
-    teshold: 1,
-    rootMargin: "-10% 0px -80%"
+    teshold: 0.9,
+    rootMargin: "-30% 0px -65% 0px"
   }
+  //rootMargin: "-10% 0px -80%"
   useEffect(() => {
     const observer = new IntersectionObserver(
       entries => {
         entries.forEach( entry => {
           if( entry.isIntersecting ) {
             navArray.forEach( (element: HTMLElement) => {
-              element.classList.remove("test")
+              element.classList.remove("navbar__current")
             });
-            if( entry.target.id !== "intro" )
-              navArray[Number(entry.target.id)].classList.toggle("test");
+            if( entry.target.id !== "intro" ) {
+              navArray[Number(entry.target.id)].classList.toggle("navbar__current");
+              sections[Number(entry.target.id)+1].classList.add("section_animation");
+            }
           }
         });
       }, options );
