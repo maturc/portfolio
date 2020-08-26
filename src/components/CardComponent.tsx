ac397ea61f6144e453f996d898ce991c2e0244bd
Matija Turčec <turcec.matija@gmail.com>
2020-08-26 17:05:58 +0200

    translation suport

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Paper, Typography, Button } from '@material-ui/core';
 import { ICard } from '../interfaces';
 
-function CardComponent( {imgUrl, imgAlt, title, body, linkToWebsite, linkToSource}:ICard ) {
+function CardComponent( {imgUrl, imgAlt, title, body, websiteButton, sourceButton, moreButton, linkToWebsite, linkToSource}: ICard ) {
   return (
     <Paper elevation={1} className="card">
       <img src={imgUrl} alt={imgAlt} className="card__img" />
@@ -14,21 +14,23 @@ function CardComponent( {imgUrl, imgAlt, title, body, linkToWebsite, linkToSourc
           {body}
         </Typography>
         <div className="card__buttons">
-          <Button
-            onClick={() => window.open(linkToWebsite, "_blank")}
-            className="button"
-            color="primary" variant="contained"
-            disableRipple={true}
-          >
-            Open Website
-          </Button>
+          { linkToWebsite &&
+            <Button
+              onClick={() => window.open(linkToWebsite, "_blank")}
+              className="button"
+              color="primary" variant="contained"
+              disableRipple={true}
+            >
+              {websiteButton}
+            </Button>
+          }
           <Button
             onClick={() => window.open(linkToSource, "_blank")}
             className="button"
             color="primary" variant="contained"
             disableRipple={true}
           >
-            Source Code
+            {linkToWebsite ? sourceButton : moreButton}
           </Button>
         </div>
       </div>
