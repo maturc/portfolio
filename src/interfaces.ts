ab56bd720c909042e5e4744e8c4ddeb1fa87f01e
Matija Turčec <turcec.matija@gmail.com>
2020-08-26 23:18:07 +0200

    interface changes

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -15,14 +15,14 @@ export type ISection = {
   //this doesn't do anything, all of it is just <any> 🙂
 }
 export type INavbar = {
-  aboutRefNav:    any;
-  skillsRefNav:   any;
-  projectsRefNav: any;
-  contactRefNav:  any;
-  aboutRef:    any;
-  skillsRef:   any;
-  projectsRef: any;
-  contactRef:  any;
+  aboutRefNav:    React.MutableRefObject<any>;
+  skillsRefNav:   React.MutableRefObject<any>;
+  projectsRefNav: React.MutableRefObject<any>;
+  contactRefNav:  React.MutableRefObject<any>;
+  aboutRef:    React.MutableRefObject<any>;
+  skillsRef:   React.MutableRefObject<any>;
+  projectsRef: React.MutableRefObject<any>;
+  contactRef:  React.MutableRefObject<any>;
   translation: any;
 }
 export type ISendEmail = {
@@ -30,4 +30,53 @@ export type ISendEmail = {
 }
 export type ISectionInView = {
   (sections: Array<HTMLElement>, navArray: Array<HTMLElement>): void;
+}
+
+//doesn't do anything, might in the future, probably won't
+type translation = {
+  language: string;
+  nav: {
+      about: string;
+      skills: string;
+      projects: string;
+      contact: string;
+  };
+  about: {
+      header: string;
+      paragraph: string;
+  };
+  skills: {
+      header: string;
+      languages: string;
+      frameworks: string;
+      databases: string;
+      technologies: string;
+      bundlers: string;
+      tools: string;
+  };
+  projects: {
+      header: string;
+      btnWebsite: string;
+      btnSource: string;
+      btnMore: string;
+      lavander: project;
+      itcomp: project;
+      algovisualize: project;
+      more: project;
+  };
+  contact: {
+      header: string;
+      form: form;
+  };
+}
+type project = {
+  header: string;
+  paragraph: string;
+  alt: string;
+}
+type form = {
+  name: string;
+  email: string;
+  message: string;
+  button: string;
 }
