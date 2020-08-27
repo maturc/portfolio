09a96a288b233ee2f377d9cedeb65d231ae22f8a
Matija Turčec <turcec.matija@gmail.com>
2020-08-27 12:18:41 +0200

    Updates

---
+++ precache-manifest.6f7d2ad1c54b179440c22efb7df4bbe1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69831960d9110999889c1124af6f04e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fbb3313622e15a7938f9",
+    "url": "/portfolio/static/css/main.a000f4e5.chunk.css"
+  },
+  {
+    "revision": "21242d4c410e9f12ca64",
+    "url": "/portfolio/static/js/2.f1bc2f96.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.f1bc2f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb3313622e15a7938f9",
+    "url": "/portfolio/static/js/main.60413097.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "35ff80bff338f688e8ddfe8d35d129a5",
+    "url": "/portfolio/static/media/algovisualize-small.35ff80bf.jpg"
+  },
+  {
+    "revision": "f9d22dc2c4b06be4bf0d27697e64dbcf",
+    "url": "/portfolio/static/media/github-small.f9d22dc2.jpg"
+  },
+  {
+    "revision": "775b804fe4471287c8f4aba16bf1f744",
+    "url": "/portfolio/static/media/itcomp-small.775b804f.jpg"
+  },
+  {
+    "revision": "9dba01131d583bc58a8e71bdbd0ae8e4",
+    "url": "/portfolio/static/media/lavander-small.9dba0113.jpg"
+  }
+]);
